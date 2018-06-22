6d32f1ba35a14085db314dab92feb60455850da0
Jbin <kjb7907@naver.com>
2018-06-23 02:17:44 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <!--<div id="nav">-->
+      <!--<router-link to="/">Home</router-link> |-->
+      <!--<router-link to="/about">About</router-link>-->
+    <!--</div>-->
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+  @import url(//fonts.googleapis.com/earlyaccess/notosanskr.css);
+  body        { font: 24px/30px 'Noto Sans KR', sans-serif; }
+  .thin       { font-weight: 100 }
+  .light      { font-weight: 300 }
+  .regular    { font-weight: 400 }
+  .medium     { font-weight: 500 }
+  .bold       { font-weight: 700 }
+  .bolder     { font-weight: 900 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
